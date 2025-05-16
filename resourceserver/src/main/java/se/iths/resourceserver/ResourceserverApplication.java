41968dafff80f1d27e0c03d3bddce7bbee5cb0c0
ArdaHalim2 <arda.halim@iths.se>
2025-05-16 10:19:01 +0200

    security config for resource server

--- resourceserver/src/main/java/se/iths/resourceserver/ResourceserverApplication.java
+++ resourceserver/src/main/java/se/iths/resourceserver/ResourceserverApplication.java
@@ -2,6 +2,12 @@ package se.iths.resourceserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 @SpringBootApplication
 public class ResourceserverApplication {
@@ -9,5 +15,21 @@ public class ResourceserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResourceserverApplication.class, args);
     }
+
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+                .sessionManagement(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/secure").hasAnyAuthority("SCOPE_read_resource")
+                        .requestMatchers("/public").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
 
 }
